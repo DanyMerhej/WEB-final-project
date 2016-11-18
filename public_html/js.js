57fc6e8c624284e1f7a844ffe6067ce921480e8a
DANNY <DANNY@DANNY-PC>
2016-11-18 03:19:22 +0200

    version 1.4

--- public_html/js.js
+++ public_html/js.js
@@ -5,3 +5,18 @@
  */
 
 
+
+
+
+   function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(33.944930899999996, 35.5983198),
+    zoom: 10
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+
+
+    
