d2660e1540194bac41faa737409a2c7c6e2dc1e4
DANNY <DANNY@DANNY-PC>
2016-11-30 23:45:12 +0200

    version 1.7

--- public_html/js.js
+++ public_html/js.js
@@ -1,39 +1,42 @@
 
- /* global element, google */
+/* global element, google */
 
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var myLatLng = {lat: 33.944930899999996, lng: 35.5983198};
-  var mapOptions = {
-    center: myLatLng,
-    zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-   var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map
-          
-        });
-}
+
+  
+
+        function myMap() {
+        var mapCanvas = document.getElementById("map");
+                var myLatLng = {lat: 33.944930899999996, lng: 35.5983198};
+                var mapOptions = {
+                center: myLatLng,
+                        zoom: 10
+                };
+                var map = new google.maps.Map(mapCanvas, mapOptions);
+                var marker = new google.maps.Marker({
+                position: myLatLng,
+                        map: map
+
+                });
+                }
+
+
+
 
 
 $(document).ready(function () {
-                $('div.hidden').fadeIn(500).removeClass('hidden');
-            });
-
-
-
-$(window).scroll(function (event) {
-        var y = $(this).scrollTop(); 
+$('div.hidden').fadeIn(500).removeClass('hidden');
+});
+        $(window).scroll(function (event) {
+var y = $(this).scrollTop();
         if (y >= 600) {
-            $('#menu').addClass('small');
-            $('#menu ul li').addClass('smallli');
-            $('#menu p a').addClass('smallp');
-        } else {
-            $('#menu').removeClass('small');
-             $('#menu ul li').removeClass('smallli');
-             $('#menu p a').removeClass('smallp');
-        }
-    });
+$('#menu').addClass('small');
+        $('#menu ul li').addClass('smallli');
+        $('#menu p a').addClass('smallp');
+} else {
+$('#menu').removeClass('small');
+        $('#menu ul li').removeClass('smallli');
+        $('#menu p a').removeClass('smallp');
+}
+});
 
     
---
+++ public_html/js2.js
@@ -0,0 +1,85 @@
+gamelength = 30;
+timerID = null;
+var playing = false;
+var numholes = 6 * 10;
+var currentpos = -1;
+function clrholes() {
+    for (var k = 0; k < document.formtable2.elements.length; k++)
+        document.formtable2.elements[k].checked = false;
+}
+function stoptimer() {
+    if (playing)
+        clearTimeout(timerID);
+}
+function showtime(remtime) {
+    document.formtable1.timeleft.value = remtime;
+    if (playing) {
+        if (remtime === 0) {
+            stopgame();
+            return;
+        }
+        else {
+            temp = remtime - 1;
+            timerID = setTimeout("showtime(temp)", 1000);
+              }
+       }
+}
+function stopgame() {
+    stoptimer();
+    playing = false;
+    document.formtable1.timeleft.value = 0;
+    clrholes();
+    display("Game Over");
+    alert('Game Over.\nYour score is :  ' + totalhits);
+}
+function play() {
+    stoptimer();
+    if (playing) {
+        stopgame();
+        return;
+    }
+    playing = true;
+    clrholes();
+    totalhits = 0;
+    document.formtable1.score.value = totalhits;
+    display("Playing ... ");
+    launch();
+    showtime(gamelength);
+}
+function display(msg) {
+    document.formtable1.state.value = msg;
+}
+function launch() {
+    var launched = false;
+    while (!launched) {
+        mynum = random();
+        if (mynum !== currentpos) {
+            document.formtable2.elements[mynum].checked = true;
+            currentpos = mynum;
+            launched = true;
+              }
+       }
+}
+ 
+function hitradio(id) {
+    if (playing === false) {
+        clrholes();
+        display("Push Start to Play");
+        return;
+    }
+    if (currentpos !== id) {
+        totalhits += -1;
+        document.formtable1.score.value = totalhits;
+        document.formtable2.elements[id].checked = false;
+    }
+    else {
+        totalhits += 1;
+        document.formtable1.score.value = totalhits;
+        launch();
+        document.formtable2.elements[id].checked = false;
+       }
+}
+ 
+function random() {
+    return(Math.floor(Math.random() * 100 % numholes));
+}
